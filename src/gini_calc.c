d22c2cd9aa4109caaf0145e816750ae66a7f1de2
Pablodadiaz <87182168+Pablodadiaz@users.noreply.github.com>
2026-04-19 12:04:01 -0300

    Refactor gini_calc function for Python integration

--- src/gini_calc.c
+++ src/gini_calc.c
@@ -1,24 +1,19 @@
-// gini_calc.c
-
 #include <stdio.h>
 
-extern long func_asm(double d1, double d2, double d3, double d4, double d5, double d6, double d7, double d8, double gini_double);
+// 1. Le avisamos a C que esta función existe en nuestro archivo Ensamblador
+extern int convertir_y_sumar(float gini);
 
-// Recibe un float (el índice GINI), lo convierte a entero y le suma 1
-// La función debe ser exportable, por eso no tiene "static"
-long procesar_gini(double gini_double) {
+// 2. Esta es la función que Python llama
+int procesar_gini(float gini_float) {
+    printf("\n  --- Llamando función en C ---\n");
+    printf("  [C] Valor float recibido de Python: %.2f\n", gini_float);
+    printf("  [C] Delegando cálculo a Ensamblador...\n");
     
-    // Paso 1: convertir float a entero (trunca los decimales)
-    // long gini_entero = (long)gini_double;
+    // 3. C ya no hace la suma. Llama a Ensamblador y le pasa el parámetro.
+    int resultado_final = convertir_y_sumar(gini_float);
     
-    // printf("[C] Valor float recibido:   %.2f\n", gini_double);
-    // printf("[C] Convertido a entero:    %ld\n", gini_entero);
+    printf("  [C] Resultado devuelto por Ensamblador (+1): %d\n\n", resultado_final);
     
-    // // Paso 2: sumar +1 como pide la consigna
-    // long resultado = gini_entero + 1;
-    
-    // printf("[C] Resultado final (+1):   %ld\n", resultado);
-    
-    // El valor de retorno queda en %rax (lo verás en la iteración 2 con GDB)
-    return func_asm(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, gini_double);
+    // 4. Devolvemos el valor final a Python
+    return resultado_final; 
 }
